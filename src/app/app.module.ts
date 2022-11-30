739f7e6eed808f837b55de8f7ed3d13266d86dee
Mizraim Martínez <mizraim.martinez@gmail.com>
2022-11-30 00:07:12 -0600

    first release

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './layout/topbar/topbar.component';
+import { PageWrapperComponent } from './layout/page-wrapper/page-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+ 
+    PageWrapperComponent
   ],
   imports: [
     BrowserModule
